0b5f3733b7ea0dce2d894ed4d29d7d87ba0672ed
usharerose <ushareroses@gmail.com>
2024-06-16 23:09:01 +0800

    feat: implement other date granularity components

--- delorean/date_utils/date_granularity.py
+++ delorean/date_utils/date_granularity.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import timedelta
 from enum import Enum
 
 
@@ -47,6 +48,71 @@ class Daily(BaseGranularity):
         end_date: datetime.date,
     ) -> int:
         return (end_date - start_date).days + 1
+
+
+class Weekly(BaseGranularity):
+
+    name = 'weekly'
+
+    def _is_partial_start(self, start_date: datetime.date) -> bool:
+        return not start_date.weekday() == 0
+
+    def _is_partial_end(self, end_date: datetime.date) -> bool:
+        return not end_date.weekday() == 6
+
+    def get_date_range_size(
+        self,
+        start_date: datetime.date,
+        end_date: datetime.date,
+    ) -> int:
+        return int(((end_date - start_date).days + 1) / 7)
+
+
+class Monthly(BaseGranularity):
+
+    name = 'monthly'
+
+    def _is_partial_start(self, start_date: datetime.date) -> bool:
+        return not start_date.day == 1
+
+    def _is_partial_end(self, end_date: datetime.date) -> bool:
+        year = end_date.year + (end_date.month + 1) // 12
+        month = (end_date.month + 1) % 12
+        next_month_first_day = datetime.date(year, month, 1)
+        last_day = (next_month_first_day + timedelta(days=-1)).day
+        return not end_date.day == last_day
+
+    def get_date_range_size(
+        self,
+        start_date: datetime.date,
+        end_date: datetime.date,
+    ) -> int:
+        start_year = start_date.year
+        end_year = end_date.year
+        if start_year == end_year:
+            return end_date.month - start_date.month + 1
+
+        year_diff = end_year - start_year - 1
+        result = (12 - start_date.month + 1) + year_diff * 12 + end_date.month
+        return result
+
+
+class Yearly(BaseGranularity):
+
+    name = 'yearly'
+
+    def _is_partial_start(self, start_date: datetime.date) -> bool:
+        return not (start_date.day == 1 and start_date.month == 1)
+
+    def _is_partial_end(self, end_date: datetime.date) -> bool:
+        return not (end_date.day == 31 and end_date.month == 12)
+
+    def get_date_range_size(
+        self,
+        start_date: datetime.date,
+        end_date: datetime.date,
+    ) -> int:
+        return end_date.year - start_date.year + 1
 
 
 class DateGranularity(Enum):
