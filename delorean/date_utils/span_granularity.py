e4c69e04782b4f246df6b67e4958cf678f2f23c8
usharerose <ushareroses@gmail.com>
2024-06-22 18:20:53 +0800

    feat: get located period's start date

--- delorean/date_utils/span_granularity.py
+++ delorean/date_utils/span_granularity.py
@@ -1,4 +1,5 @@
 import datetime
+from datetime import timedelta
 from enum import Enum
 
 from delorean.date_utils.common import (
@@ -32,6 +33,25 @@ class BaseGranularity:
             )
         return func(date_range.start_date)
 
+    def get_first_period_located_start_date(
+        self,
+        date_range: DateRange
+    ) -> datetime.date:
+        """
+        get the start date of date period that date range's first period located
+        can only support finer DateGranularity than SpanGranularity
+
+        private methods should be named as '_get_%(date_granularity_name)s_located_start_date'
+        """
+        granularity_name = date_range.date_granularity.name.lower()
+        try:
+            func = getattr(self, f'_get_{granularity_name}_located_start_date')
+        except AttributeError:
+            raise NotImplementedError(
+                f'get_location on {granularity_name} has not been implemented'
+            )
+        return func(date_range.start_date)
+
 
 class Daily(BaseGranularity):
 
@@ -42,6 +62,12 @@ class Daily(BaseGranularity):
         a_date: datetime.date,  # NOQA
     ) -> int:
         return 0
+
+    def _get_daily_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return a_date
 
 
 class Weekly(BaseGranularity):
@@ -59,6 +85,19 @@ class Weekly(BaseGranularity):
         a_date: datetime.date,  # NOQA
     ) -> int:
         return 0
+
+    def _get_daily_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        weekday = a_date.weekday()
+        return a_date - timedelta(days=weekday)
+
+    def _get_weekly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return a_date
 
 
 class Monthly(BaseGranularity):
@@ -84,6 +123,26 @@ class Monthly(BaseGranularity):
         a_date: datetime.date,  # NOQA
     ) -> int:
         return 0
+
+    def _get_daily_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return datetime.date(a_date.year, a_date.month, 1)
+
+    def _get_weekly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        key_date = get_week_anchor_date(a_date)
+        first_week_start_date = get_start_date_of_monthly_start_week(key_date.year, key_date.month)
+        return first_week_start_date
+
+    def _get_monthly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return a_date
 
 
 class Yearly(BaseGranularity):
@@ -116,6 +175,32 @@ class Yearly(BaseGranularity):
     ) -> int:
         return 0
 
+    def _get_daily_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return datetime.date(a_date.year, 1, 1)
+
+    def _get_weekly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        key_date = get_week_anchor_date(a_date)
+        first_week_start_date = get_start_date_of_monthly_start_week(key_date.year, 1)
+        return first_week_start_date
+
+    def _get_monthly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return datetime.date(a_date.year, 1, 1)
+
+    def _get_yearly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return a_date
+
 
 class Periodic(BaseGranularity):
 
@@ -144,6 +229,32 @@ class Periodic(BaseGranularity):
         a_date: datetime.date,  # NOQA
     ) -> int:
         return 0
+
+    def _get_daily_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return a_date
+
+    def _get_weekly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        key_date = get_week_anchor_date(a_date)
+        first_week_start_date = get_start_date_of_monthly_start_week(key_date.year, 1)
+        return a_date
+
+    def _get_monthly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return a_date
+
+    def _get_yearly_located_start_date(  # NOQA
+        self,
+        a_date: datetime.date,
+    ) -> datetime.date:
+        return a_date
 
 
 class SpanGranularity(Enum):
