133432342081fd2221418c5da6dc7c40b5d81109
usharerose <ushareroses@gmail.com>
2024-06-22 18:21:31 +0800

    test: unittest on get_first_period_located_start_date

--- tests/unittest/date_utils/test_span_granularity.py
+++ tests/unittest/date_utils/test_span_granularity.py
@@ -33,6 +33,19 @@ class SpanGranularityDailyTestCase(TestCase):
             0,
         )
 
+    def test_daily_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 6, 18)
+        end_date = datetime.date(2024, 6, 21)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.DAILY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 6, 18),
+        )
+
     def test_unsupported_weekly_date_granularity(self):
         start_date = datetime.date(2024, 6, 17)
         end_date = datetime.date(2024, 6, 23)
@@ -43,6 +56,8 @@ class SpanGranularityDailyTestCase(TestCase):
         )
         with self.assertRaises(NotImplementedError):
             self.granularity.get_first_period_index(date_range)
+        with self.assertRaises(NotImplementedError):
+            self.granularity.get_first_period_located_start_date(date_range)
 
     def test_unsupported_monthly_date_granularity(self):
         start_date = datetime.date(2024, 1, 1)
@@ -54,6 +69,8 @@ class SpanGranularityDailyTestCase(TestCase):
         )
         with self.assertRaises(NotImplementedError):
             self.granularity.get_first_period_index(date_range)
+        with self.assertRaises(NotImplementedError):
+            self.granularity.get_first_period_located_start_date(date_range)
 
     def test_unsupported_yearly_date_granularity(self):
         start_date = datetime.date(2024, 1, 1)
@@ -65,6 +82,8 @@ class SpanGranularityDailyTestCase(TestCase):
         )
         with self.assertRaises(NotImplementedError):
             self.granularity.get_first_period_index(date_range)
+        with self.assertRaises(NotImplementedError):
+            self.granularity.get_first_period_located_start_date(date_range)
 
 
 class SpanGranularityWeeklyTestCase(TestCase):
@@ -88,6 +107,19 @@ class SpanGranularityWeeklyTestCase(TestCase):
             1,
         )
 
+    def test_daily_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 6, 18)
+        end_date = datetime.date(2024, 6, 21)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.DAILY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 6, 17),
+        )
+
     def test_weekly_first_period_index(self):
         start_date = datetime.date(2024, 6, 3)
         end_date = datetime.date(2024, 6, 23)
@@ -101,6 +133,19 @@ class SpanGranularityWeeklyTestCase(TestCase):
             0,
         )
 
+    def test_weekly_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 6, 3)
+        end_date = datetime.date(2024, 6, 23)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.WEEKLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 6, 3),
+        )
+
     def test_unsupported_monthly_date_granularity(self):
         start_date = datetime.date(2024, 1, 1)
         end_date = datetime.date(2024, 1, 31)
@@ -111,6 +156,8 @@ class SpanGranularityWeeklyTestCase(TestCase):
         )
         with self.assertRaises(NotImplementedError):
             self.granularity.get_first_period_index(date_range)
+        with self.assertRaises(NotImplementedError):
+            self.granularity.get_first_period_located_start_date(date_range)
 
     def test_unsupported_yearly_date_granularity(self):
         start_date = datetime.date(2024, 1, 1)
@@ -122,6 +169,8 @@ class SpanGranularityWeeklyTestCase(TestCase):
         )
         with self.assertRaises(NotImplementedError):
             self.granularity.get_first_period_index(date_range)
+        with self.assertRaises(NotImplementedError):
+            self.granularity.get_first_period_located_start_date(date_range)
 
 
 class SpanGranularityMonthlyTestCase(TestCase):
@@ -143,6 +192,19 @@ class SpanGranularityMonthlyTestCase(TestCase):
         self.assertEqual(
             self.granularity.get_first_period_index(date_range),
             17,
+        )
+
+    def test_daily_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 6, 18)
+        end_date = datetime.date(2024, 6, 21)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.DAILY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 6, 1),
         )
 
     def test_daily_first_period_index_in_leap_year_feb(self):
@@ -171,6 +233,19 @@ class SpanGranularityMonthlyTestCase(TestCase):
             4,
         )
 
+    def test_weekly_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 2, 26)
+        end_date = datetime.date(2024, 3, 10)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.WEEKLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 1, 29),
+        )
+
     def test_weekly_first_period_index_at_another_month(self):
         start_date = datetime.date(2024, 4, 29)
         end_date = datetime.date(2024, 5, 12)
@@ -182,6 +257,19 @@ class SpanGranularityMonthlyTestCase(TestCase):
         self.assertEqual(
             self.granularity.get_first_period_index(date_range),
             0,
+        )
+
+    def test_weekly_first_period_located_start_date_at_another_month(self):
+        start_date = datetime.date(2024, 4, 29)
+        end_date = datetime.date(2024, 5, 12)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.WEEKLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 4, 29),
         )
 
     def test_monthly_first_period_index(self):
@@ -197,6 +285,19 @@ class SpanGranularityMonthlyTestCase(TestCase):
             0,
         )
 
+    def test_monthly_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 1, 1)
+        end_date = datetime.date(2024, 1, 31)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.MONTHLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 1, 1),
+        )
+
     def test_unsupported_yearly_date_granularity(self):
         start_date = datetime.date(2024, 1, 1)
         end_date = datetime.date(2024, 12, 31)
@@ -207,6 +308,8 @@ class SpanGranularityMonthlyTestCase(TestCase):
         )
         with self.assertRaises(NotImplementedError):
             self.granularity.get_first_period_index(date_range)
+        with self.assertRaises(NotImplementedError):
+            self.granularity.get_first_period_located_start_date(date_range)
 
 
 class SpanGranularityYearlyTestCase(TestCase):
@@ -230,6 +333,19 @@ class SpanGranularityYearlyTestCase(TestCase):
             169,
         )
 
+    def test_daily_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 6, 18)
+        end_date = datetime.date(2024, 6, 21)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.DAILY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 1, 1),
+        )
+
     def test_daily_first_period_index_in_leap_year(self):
         start_date = datetime.date(2024, 12, 31)
         end_date = datetime.date(2024, 12, 31)
@@ -241,6 +357,19 @@ class SpanGranularityYearlyTestCase(TestCase):
         self.assertEqual(
             self.granularity.get_first_period_index(date_range),
             365,
+        )
+
+    def test_daily_first_period_located_start_date_in_leap_year(self):
+        start_date = datetime.date(2024, 12, 31)
+        end_date = datetime.date(2024, 12, 31)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.DAILY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 1, 1),
         )
 
     def test_weekly_first_period_index(self):
@@ -256,6 +385,19 @@ class SpanGranularityYearlyTestCase(TestCase):
             23,
         )
 
+    def test_weekly_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 6, 10)
+        end_date = datetime.date(2024, 6, 23)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.WEEKLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 1, 1),
+        )
+
     def test_weekly_first_period_index_at_another_year(self):
         start_date = datetime.date(2018, 12, 31)
         end_date = datetime.date(2019, 1, 6)
@@ -267,6 +409,19 @@ class SpanGranularityYearlyTestCase(TestCase):
         self.assertEqual(
             self.granularity.get_first_period_index(date_range),
             0,
+        )
+
+    def test_weekly_first_period_located_start_date_at_another_year(self):
+        start_date = datetime.date(2018, 12, 31)
+        end_date = datetime.date(2019, 1, 6)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.WEEKLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2018, 12, 31),
         )
 
     def test_weekly_first_period_index_at_fifty_week_year(self):
@@ -282,6 +437,19 @@ class SpanGranularityYearlyTestCase(TestCase):
             52,
         )
 
+    def test_weekly_first_period_located_start_date_at_fifty_week_year(self):
+        start_date = datetime.date(2015, 12, 28)
+        end_date = datetime.date(2016, 1, 3)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.WEEKLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2014, 12, 29),
+        )
+
     def test_monthly_first_period_index(self):
         start_date = datetime.date(2023, 12, 1)
         end_date = datetime.date(2024, 6, 30)
@@ -295,6 +463,19 @@ class SpanGranularityYearlyTestCase(TestCase):
             11,
         )
 
+    def test_monthly_first_period_located_start_date(self):
+        start_date = datetime.date(2023, 12, 1)
+        end_date = datetime.date(2024, 6, 30)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.MONTHLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2023, 1, 1),
+        )
+
     def test_yearly_first_period_index(self):
         start_date = datetime.date(2023, 1, 1)
         end_date = datetime.date(2024, 12, 31)
@@ -306,6 +487,19 @@ class SpanGranularityYearlyTestCase(TestCase):
         self.assertEqual(
             self.granularity.get_first_period_index(date_range),
             0,
+        )
+
+    def test_yearly_first_period_located_start_date(self):
+        start_date = datetime.date(2023, 1, 1)
+        end_date = datetime.date(2024, 12, 31)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.YEARLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2023, 1, 1),
         )
 
 
@@ -330,6 +524,19 @@ class SpanGranularityPeriodicTestCase(TestCase):
             0,
         )
 
+    def test_daily_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 6, 18)
+        end_date = datetime.date(2024, 6, 21)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.DAILY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 6, 18),
+        )
+
     def test_weekly_first_period_index(self):
         start_date = datetime.date(2024, 6, 10)
         end_date = datetime.date(2024, 6, 23)
@@ -341,6 +548,19 @@ class SpanGranularityPeriodicTestCase(TestCase):
         self.assertEqual(
             self.granularity.get_first_period_index(date_range),
             0,
+        )
+
+    def test_weekly_first_period_located_start_date(self):
+        start_date = datetime.date(2024, 6, 10)
+        end_date = datetime.date(2024, 6, 23)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.WEEKLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2024, 6, 10),
         )
 
     def test_monthly_first_period_index(self):
@@ -356,6 +576,19 @@ class SpanGranularityPeriodicTestCase(TestCase):
             0,
         )
 
+    def test_monthly_first_period_located_start_date(self):
+        start_date = datetime.date(2023, 12, 1)
+        end_date = datetime.date(2024, 6, 30)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.MONTHLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2023, 12, 1),
+        )
+
     def test_yearly_first_period_index(self):
         start_date = datetime.date(2023, 1, 1)
         end_date = datetime.date(2024, 12, 31)
@@ -367,4 +600,17 @@ class SpanGranularityPeriodicTestCase(TestCase):
         self.assertEqual(
             self.granularity.get_first_period_index(date_range),
             0,
+        )
+
+    def test_yearly_first_period_located_start_date(self):
+        start_date = datetime.date(2023, 1, 1)
+        end_date = datetime.date(2024, 12, 31)
+        date_range = DateRange(
+            start_date,
+            end_date,
+            DateGranularity.YEARLY,
+        )
+        self.assertEqual(
+            self.granularity.get_first_period_located_start_date(date_range),
+            datetime.date(2023, 1, 1),
         )
