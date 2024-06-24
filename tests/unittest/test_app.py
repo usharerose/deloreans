84e791d0cfa979d684e6e1bf034b6fb1304d4e6f
usharerose <ushareroses@gmail.com>
2024-06-25 00:18:37 +0800

    feat: calculate compared date range's start and end date

--- tests/unittest/test_app.py
+++ tests/unittest/test_app.py
@@ -24,3 +24,21 @@ class DeLoreanTestCase(TestCase):
             span_count,
             span_granularity,
         ))
+
+    def test_get_compared_date_range(self):
+        start_date = datetime.date(2024, 6, 10)
+        end_date = datetime.date(2024, 6, 16)
+        date_granularity = DateGranularity.DAILY
+        span_count = 3
+        span_granularity = SpanGranularity.DAILY
+        executor = DeLorean(
+            start_date,
+            end_date,
+            date_granularity,
+            span_count,
+            span_granularity,
+        )
+        self.assertEqual(
+            executor.get(),
+            (datetime.date(2024, 6, 13), datetime.date(2024, 6, 19))
+        )
