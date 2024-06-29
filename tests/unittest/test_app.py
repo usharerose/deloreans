41e1a8b6446dae13e7c1775952919a81dab6cf26
usharerose <ushareroses@gmail.com>
2024-06-29 23:23:17 +0800

    test: get compared date range with various firstweekday

--- tests/unittest/test_app.py
+++ tests/unittest/test_app.py
@@ -42,3 +42,23 @@ class DeLoreansTestCase(TestCase):
             executor.get(),
             (datetime.date(2024, 6, 13), datetime.date(2024, 6, 19))
         )
+
+    def test_get_compared_date_range_with_given_firstweekday(self):
+        start_date = datetime.date(2024, 2, 11)
+        end_date = datetime.date(2024, 2, 24)
+        date_granularity = DateGranularity.WEEKLY
+        offset = -2
+        offset_granularity = OffsetGranularity.MONTHLY
+        firstweekday = 6
+        executor = DeLoreans(
+            start_date,
+            end_date,
+            date_granularity,
+            offset,
+            offset_granularity,
+            firstweekday,
+        )
+        self.assertEqual(
+            executor.get(),
+            (datetime.date(2023, 12, 10), datetime.date(2023, 12, 23))
+        )
